3808d9056c8fd3543712034d875924695024b277
root <root@at-node20.ad.bcm.edu>
2022-03-11 23:10:12 +0000

    imporved limb functions exported to graph

--- python_tools/function_utils.py
+++ python_tools/function_utils.py
@@ -28,3 +28,49 @@ def all_functions_from_module(
         return [k[1] for k in return_value]
         
     return return_value
+
+
+def rename(newname):
+    """
+    Can be used as a decorator to rename the functions
+    """
+    def decorator(f):
+        f.__name__ = newname
+        return f
+    return decorator
+
+
+
+'''
+"""
+Demo on how to create funcions programmatically,
+have to create a wrapper function and call that 
+with the parameters you want to signal the 
+binding then, otherwise they will do late binding
+
+"""
+
+--Example 1--
+my_list = []
+for i in range(0,3):
+    def make_f(i):
+        def f():
+            return i
+        return f
+    my_list.append(make)
+
+--Example 2 ---
+def myfunc1(x):
+    print("myfunc1") 
+def myfunc2(x):
+    print("myfunc2") 
+
+new_funcs = []
+for f in [myfunc1,myfunc2]:
+    def make_func(f):
+        def func_wrapper(str1="hi",str2="hello"):
+            return str1,str2,f
+        return func_wrapper
+    new_funcs.append(make_func(f))
+    
+new_funcs'''
