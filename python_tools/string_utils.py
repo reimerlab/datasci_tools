491af1433391314dc6141546281ae68248507aee
root <root@at-node20.ad.bcm.edu>
2022-05-11 22:32:57 +0000

    worked on visualizing cell type embedding

--- python_tools/string_utils.py
+++ python_tools/string_utils.py
@@ -54,3 +54,7 @@ def str_overlap(s1, s2):
     s = difflib.SequenceMatcher(None, s1, s2)
     pos_a, pos_b, size = s.find_longest_match(0, len(s1), 0, len(s2)) 
     return s1[pos_a:pos_a+size]
+
+
+def is_int(s):
+    return s.isdigit()
