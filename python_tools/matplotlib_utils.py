e80505110c61d4d125d78ed2f9e4c9b3758e565c
root <bac8@rice.edu>
2023-01-14 00:25:38 +0000

    2by2 plots

--- python_tools/matplotlib_utils.py
+++ python_tools/matplotlib_utils.py
@@ -2582,4 +2582,67 @@ def scale_axes_lim_isotropic(
 
     return ax
 
+def mappable_from_ax(ax):
+    return ax.collections[0]
+
+def plot_confusion_matrix(
+    cf_matrix,
+    annot = True,
+    annot_fontsize = 30,
+    cell_fmt = ".2f",
+    cmap = "Blues",
+    vmin = 0,
+    vmax = 1,
+    
+    #argmuments for axes 
+    axes_font_size = 20,
+    xlabel_rotation = 15,
+    ylabel_rotation = 0,
+    
+    xlabels = None,
+    ylabels = None,
+    
+    #colorbar 
+    plot_colorbar = True,
+    colobar_tick_fontsize = 25,
+    
+    ax = None,
+    ):
+#     if vmax == 1 and np.max(cf_matrix) > 1:
+#         cf_matrix = normalize_confusion_matrix(cf_matrix)
+    
+    ax = sns.heatmap(
+        cf_matrix,
+        annot=annot,
+        fmt = cell_fmt,
+        annot_kws={
+            "fontsize":annot_fontsize,
+        },
+        cbar = plot_colorbar,
+        cmap = cmap,
+        vmin=vmin, 
+        vmax=vmax,
+        ax = ax,
+    )
+
+    ax = mu.set_axes_font_size(
+        ax,
+        axes_font_size,
+        x_rotation=xlabel_rotation,
+        y_rotation = ylabel_rotation)
+    
+    if xlabels is not None or ylabels is not None:
+        mu.set_axes_ticklabels(ax,xlabels,ylabels)
+        
+    if plot_colorbar:
+        cbar = ax.collections[0].colorbar
+        cbar.ax.tick_params(labelsize=colobar_tick_fontsize)
+
+    return ax
+
+def set_frame_off(ax):
+    ax.set_frame_on(False)
+    
+hide_frame = set_frame_off
+
 import matplotlib_utils as mu
