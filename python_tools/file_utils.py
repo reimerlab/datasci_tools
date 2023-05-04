aae4cca3e2e6bf63ccec64d469b60e901e3a82fa
root <bac8@rice.edu>
2023-05-04 22:51:32 +0000

    added requiremenet_functionality

--- python_tools/file_utils.py
+++ python_tools/file_utils.py
@@ -38,3 +38,45 @@ def file_from_dicts(
             f.write(line_to_write)
             
     return filepath
+
+
+import os
+def str_in_filepath(
+    search_str,
+    filepath,
+    dir_path = None,
+    verbose = True,
+    ):
+    
+    if dir_path is not None:
+        filepath = os.path.join(dir_path, filepath)
+    cur_path = filepath
+    
+    if os.path.isfile(cur_path):
+        try:
+            with open(cur_path, 'r') as file:
+                # read all content of a file and search string
+                if search_str in file.read():
+                    if verbose:
+                        print(f'{search_str} found in {cur_path}')
+                    return True
+        except:
+            if verbose:
+                print(f"    Wasn't able to read {cur_path}")
+    return False
+        
+    
+def search_directory_files_for_str(
+    dir_path,
+    search_str,
+    verbose = True):
+    
+    all_files = []
+    # iterate each file in a directory
+    for file in os.listdir(dir_path):
+        cur_path = os.path.join(dir_path, file)
+        # check if it is a file
+        if str_in_filepath(search_str,cur_path,verbose = verbose):
+            all_files.append(cur_path)
+            
+    return all_files
